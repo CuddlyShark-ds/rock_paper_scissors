5066884a19300d2aada33176de7a69d688931f3c
CuddlyShark <135768450+CuddlyShark-ds@users.noreply.github.com>
2024-11-27 11:49:13 -0500

    wrote code to get computer random choice and get player choice

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-// get a random choice for the computer
+let available_choices = ["rock", "paper", "scissors"]
+let comp_choice = null
+let player_choice = null
+
+function main(){
+    // get a random choice for the computer
+    let comp_choice = () => Math.floor(Math.random() * (0, 3))
+}
 
 // have the player choose rock, paper or scissor
-
+function player_click(choice) {
+    console.log(choice)
+}
 // compare the two choices
 
 // announce a winner
+
+main()
